65a01a67d6ecabf963fdb347790c1440a1a55211
shifafatiyma19-ux <shifafatiyma19@gmail.com>
2026-08-21 16:04:36 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,43 @@
-const CACHE_NAME = "dearme-v2";
+const CACHE_NAME = "dearme-v3";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./manifest.json",
-    "./icons/icon-192.png",
-    "./icons/icon-512.png"
+    "./style.css",
+    "./script.js",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
+        caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(FILES_TO_CACHE);
         })
     );
+
     self.skipWaiting();
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", (event) => {
     event.waitUntil(
-        caches.keys().then(function(cacheNames) {
+        caches.keys().then((cacheNames) => {
             return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
             );
         })
     );
+
     self.clients.claim();
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
         })
     );
 });
